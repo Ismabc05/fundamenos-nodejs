767e93323f43b09b7bb4687cb69a182f3ed912bd
Ismael <ismael@example.com>
2026-04-08 13:00:55 +0200

    importacion-exportacion

---
+++ src/commonjs/main.js
@@ -0,0 +1,4 @@
+const math = require("./math");
+
+console.log(`Sumar: ${math.add(2, 3)}` )
+console.log(`Restar: ${math.subtract(5, 2)}` )
---
+++ src/modulo-terceros/third-party.js
@@ -0,0 +1,4 @@
+const idOdd = require("is-odd")
+
+console.log(idOdd("1"))
+console.log(idOdd("3"))
